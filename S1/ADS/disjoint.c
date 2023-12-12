82bbad37896f1dfff7fdcb6609389ab1ff8d0cf1
ADITH-TM <150991111+ADITH-TM@users.noreply.github.com>
2023-12-12 08:54:09 +0000

    Update disjoint.c

--- S1/ADS/disjoint.c
+++ S1/ADS/disjoint.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node {
+struct node
+{
 struct node *rep;
 struct node *next;
 int data;
@@ -14,10 +15,12 @@ new->data=x;
 heads[countroot]=new;
 tails[countroot++]=new;
 }
-struct node* find(int a) {
+struct node* find(int a)
+{
 int i;
 struct node *tmp=(struct node*)malloc(sizeof(struct node));
-for(i=0;i<countroot;i++) {
+for(i=0;i<countroot;i++)
+  {
 tmp=heads[i];
 while(tmp!=NULL) {
 if(tmp->data==a)
@@ -27,22 +30,27 @@ tmp=tmp->next;
 }
 return NULL;
 }
-void unionsets(int a,int b) {
+void unionsets(int a,int b) 
+{
 int i,pos,flag=0,j;
 struct node *tail2=(struct node*)malloc(sizeof(struct node));
 struct node *rep1=find(a);
 struct node *rep2=find(b);
-if(rep1==NULL || rep2==NULL) {
+if(rep1==NULL || rep2==NULL) 
+{
 printf("\nNot present");
 return;
 }
 if(rep1!=rep2) {
-for(j=0;j<countroot;j++) {
-if(heads[j]==rep2) {
+for(j=0;j<countroot;j++)
+  {
+if(heads[j]==rep2)
+{
 pos=j;
 flag=1;
 countroot-=1;tail2=tails[j];
-for(i=pos;i<countroot;i++) {
+for(i=pos;i<countroot;i++)
+  {
 heads[i]=heads[i+1];
 tails[i]=tails[i+1];
 }
@@ -50,20 +58,23 @@ tails[i]=tails[i+1];
 if(flag==1)
 break;
 }
-for(j=0;j<countroot;j++) {
+for(j=0;j<countroot;j++)
+  {
 if(heads[j]==rep1) {
 tails[j] ->next=rep2;
 tails[j]=tail2;
 break;
 }
 }
-while(rep2!=NULL) {
+while(rep2!=NULL)
+  {
 rep2->rep=rep1;
 rep2=rep2->next;
 }
 }
 }
-int search(int x) {
+int search(int x)
+{
 int i;
 struct node *tmp=(struct node*)malloc(sizeof(struct node));
 for(i=0;i<countroot;i++) {
@@ -78,17 +89,20 @@ tmp=tmp->next;
 }
 return 0;
 }
-void main() {
+void main()
+{
 int c,x,y,i;
 struct node *rep=(struct node*)malloc(sizeof(struct node));
-while(1) {
+while(1) 
+{
 printf("\n\n1:Make Set");
 printf("\n2:Display set Representatives");
 printf("\n3:Union");
 printf("\n4:Find Set");
 printf("\n5:Exit");printf("\nEnter your choice : ");
 scanf("%d",&c);
-switch(c) {
+switch(c) 
+{
 case 1:
 printf("Enter element : ");
 scanf("%d",&x);
